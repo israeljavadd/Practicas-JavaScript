0d35b1fb4362bee630895951dc7a8b03f28fec21
Israeljavadd <israeljavadd@gmail.com>
2022-04-03 17:31:33 -0500

    JavaScript

---
+++ 3/carrosScript.js
@@ -0,0 +1,16 @@
+
+            let marca,modelo,descuento = 0;
+            marca= prompt("Marca?","0");
+            modelo= prompt("Modelo?","0");
+            if((marca=="LA Ferrari")&&(modelo=="F355"))
+        {
+        documento=5;
+
+        }
+        if((marca=="Chevrolet")&&(modelo=="Onix"))
+        {
+            descuento=10;
+        }
+        document.write("<h3>Su descuento es ${descuento}</h3>");
+        
+        
---
+++ 5/saludo.js
@@ -0,0 +1,5 @@
+function saludo()
+{
+    let nom_usuario = document.fdatos.entrada.value;
+    document.writeln("Hola, buen dia "+nom_usuario);
+}
